e2d0f534f7b599ca5eee9ce10edaa2df1e96cf30
Eric Brengle <eric.brengle@gmail.com>
2015-03-26 16:25:02 -0500

    converted clock to 12 hour time picker with AM/PM. A little buggy still

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,32 +1,6 @@
 //Creating the module named scheduler
 var scheduler = angular.module('scheduler', ['ngRoute', 'ui.router', 'mgcrea.ngStrap', ]);
 
-//configuring my routes for view of partials
-// scheduler.config(function($routeProvider){
-//   $routeProvider
-//
-//     //this is my homepage partial
-//     .when('/', {
-//       templateUrl : 'partials/basic.html',
-//       controller  : 'mainController',
-//       controllerAs: 'appointment'
-//     })
-//
-//     //this is my appointment time view
-//     .when('/selectappt', {
-//       templateUrl : 'partials/selectappointment.html',
-//       controller  : 'mainController',
-//       ControllerAs: 'appointment'
-//     })
-//
-//     //this is my confirmation page
-//     .when('/confirmation', {
-//       templateUrl : 'partials/confirmation.html',
-//       controller  : 'mainController',
-//       ControllerAs: 'appointment'
-//     });
-// });
-
 scheduler.config(function($stateProvider, $urlRouterProvider){
 
   $urlRouterProvider.otherwise("/form/basic");
@@ -58,53 +32,50 @@ scheduler.config(function($stateProvider, $urlRouterProvider){
 //this is the controller for my app
 scheduler.controller('mainController', function($scope) {
 
-  $scope.appointment = {};
+   $scope.appointment = {};
 
+   $scope.setAvailableTimes = function () {
+     var dayOfWeek = $scope.appointment.selectedDate.date.getDay();
 
+     switch (dayOfWeek) {
+       case 0:
+         $scope.minTime = '12:00 AM';
+         $scope.maxTime = '12:00 AM';
+         break;
+       case 1:
+         $scope.minTime = '12:00 AM';
+         $scope.maxTime = '12:00 AM';
+         break;
+       case 2:
+         $scope.minTime = '12:00 PM';
+         $scope.maxTime = '7:00 PM';
+         break;
+       case 3:
+         $scope.minTime = '8:00 AM';
+         $scope.maxTime = '1:00 PM';
+         break;
+       case 4:
+         $scope.minTime = '12:00 PM';
+         $scope.maxTime = '7:00 PM';
+         break;
+       case 5:
+         $scope.minTime = '8:00 AM';
+         $scope.maxTime = '1:00 PM';
+         break;
+       case 6:
+         $scope.minTime = '9:00 AM';
+         $scope.maxTime = '12:00 PM';
+         break;
+     }
 
+     $scope.appointment.selectedDate.date.setHours($scope.minTime.split(':')[0]);
+     $scope.appointment.selectedDate.date.setMinutes('00');
+     $scope.appointment.selectedDate.date.setSeconds('00');
 
- $scope.setAvailableTimes = function () {
-   var dayOfWeek = $scope.appointment.selectedDate.date.getDay();
-
-   switch (dayOfWeek) {
-     case 0:
-       $scope.minTime = '00:00';
-       $scope.maxTime = '00:00';
-       break;
-     case 1:
-       $scope.minTime = '00:00';
-       $scope.maxTime = '00:00';
-       break;
-     case 2:
-       $scope.minTime = '12:00';
-       $scope.maxTime = '19:00';
-       break;
-     case 3:
-       $scope.minTime = '08:00';
-       $scope.maxTime = '13:00';
-       break;
-     case 4:
-       $scope.minTime = '12:00';
-       $scope.maxTime = '19:00';
-       break;
-     case 5:
-       $scope.minTime = '08:00';
-       $scope.maxTime = '13:00';
-       break;
-     case 6:
-       $scope.minTime = '09:00';
-       $scope.maxTime = '12:00';
-       break;
    }
 
-   $scope.appointment.selectedDate.date.setHours($scope.minTime.split(':')[0]);
-   $scope.appointment.selectedDate.date.setMinutes('00');
-   $scope.appointment.selectedDate.date.setSeconds('00');
-
- }
-
- $scope.selectedTime = function () {
-   console.log($scope.appointment.selectedDate.date);
- }
+   $scope.selectedTime = function () {
+     console.log($scope.appointment.selectedDate.date);
+   }
 
 });
